6f2343ff627eb599ced641ffddac457aa7359494
Steven Kristian <stevenkristian300900@gmail.com>
2022-09-18 13:36:13 +0700

    initialize repository

---
+++ src/main/java/com/monevi/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.monevi.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+  @GetMapping("/")
+  public String helloWorld() {
+    return "Hello World!";
+  }
+}
