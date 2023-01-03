7d06eaa2443abd61ffb3c60324c6221e53519e6f
Sofie Adira Chrysanthia <sofie.chrysanthia@gdn-commerce.com>
2023-01-03 20:54:41 +0700

    change student registration validation

--- src/main/java/com/monevi/controller/ApiPath.java
+++ src/main/java/com/monevi/controller/ApiPath.java
@@ -13,7 +13,7 @@ public class ApiPath {
   public static final String REPORT = "/report";
   public static final String PROGRAM = "/program";
   public static final String USER = "/user";
-  public static final String AUTH = USER + "auth";
+  public static final String AUTH = USER + "/auth";
 
   public static final String FIND_ALL = "/all";
   public static final String CREATE_NEW = "/create-new";
--- src/main/java/com/monevi/repository/UserAccountRepository.java
+++ src/main/java/com/monevi/repository/UserAccountRepository.java
@@ -1,14 +1,18 @@
 package com.monevi.repository;
 
 import com.monevi.entity.UserAccount;
+import com.monevi.enums.UserAccountRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, String>  {
-    Optional<UserAccount> findByNimAndPeriodMonthAndPeriodYearAndMarkForDeleteIsFalse(String nim, Integer periodMonth, Integer periodYear);
-    Optional<UserAccount> findByEmailAndPeriodMonthAndPeriodYearAndMarkForDeleteFalse(String email, Integer periodMonth, Integer periodYear);
-    Optional<UserAccount> findByEmailAndMarkForDeleteFalse(String email);
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+  Optional<UserAccount> findByNimAndMarkForDeleteIsFalse(String nim);
 
-    Optional<UserAccount> findByIdAndMarkForDeleteIsFalse(String id);
+  Optional<UserAccount> findByPeriodMonthAndPeriodYearAndRoleAndLockedAccountFalseAndMarkForDeleteFalse(
+      Integer periodMonth, Integer periodYear, UserAccountRole role);
+
+  Optional<UserAccount> findByEmailAndMarkForDeleteFalse(String email);
+
+  Optional<UserAccount> findByIdAndMarkForDeleteIsFalse(String id);
 }
